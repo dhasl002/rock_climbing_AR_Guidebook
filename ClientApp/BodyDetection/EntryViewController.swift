88248441c2c5f2e6213693ad7c7fb152010c6cb2
dhasl002 <dhasl002@odu.edu>
2019-10-01 22:02:56 -0700

    1st view controller finished

--- ClientApp/BodyDetection/EntryViewController.swift
+++ ClientApp/BodyDetection/EntryViewController.swift
@@ -12,9 +12,19 @@ import Foundation
 class EntryViewController: UIViewController {
     @IBOutlet var mainView: UIView!
     @IBOutlet var entryButton: UIButton!
+    @IBOutlet var titleView1: UIView!
+    @IBOutlet var titleView2: UIView!
     
     override func viewDidLoad() {
         entryButton.layer.cornerRadius = 30
         entryButton.clipsToBounds = true
+        titleView1.layer.cornerRadius = 40
+        titleView1.clipsToBounds = true
+        titleView2.layer.cornerRadius = 40
+        titleView2.clipsToBounds = true
+    }
+    
+    @IBAction func buttonPressed() {
+        let vc = ViewController()
     }
 }
